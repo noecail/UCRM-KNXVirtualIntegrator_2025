72be58e7294ed78f51629785d89e958cdd2afa6a
MaximeO <maxime.oliveira40@gmail.com>
2024-08-05 15:41:12 +0200

    .

---
+++ KNX Virtual Integrator/KNX Virtual Integrator/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KNX_Virtual_Integrator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
