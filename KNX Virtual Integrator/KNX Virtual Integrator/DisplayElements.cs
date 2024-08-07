5274ddd712180e84e70075f2722a2584cf90f290
MaximeO <maxime.oliveira40@gmail.com>
2024-08-07 16:29:29 +0200

    Récupération des morceaux de KNX Boost qui seront utiles à ce programme

---
+++ KNX Virtual Integrator/KNX Virtual Integrator/DisplayElements.cs
@@ -0,0 +1,38 @@
+﻿namespace KNX_Virtual_Integrator;
+
+public class DisplayElements
+{
+    /* ------------------------------------------------------------------------------------------------
+    ------------------------------------------- ATTRIBUTS  --------------------------------------------
+    ------------------------------------------------------------------------------------------------ */
+    /// <summary>
+    /// Represents the main window instance of the application.
+    /// </summary>
+    public MainWindow MainWindow { get; } = new();
+        
+    /// <summary>
+    /// Represents the settings window instance where application settings are configured.
+    /// </summary>
+    public SettingsWindow? SettingsWindow { get; } = new();
+    
+        
+    /* ------------------------------------------------------------------------------------------------
+    -------------------------------------------- METHODES  --------------------------------------------
+    ------------------------------------------------------------------------------------------------ */
+    /// <summary>
+    /// Shows the main window of the application.
+    /// </summary>
+    public void ShowMainWindow()
+    {
+        MainWindow.Show();
+    }
+        
+        
+    /// <summary>
+    /// Shows the settings window of the application if it is available.
+    /// </summary>
+    public void ShowSettingsWindow()
+    {
+        SettingsWindow?.Show();
+    }
+} 
--- KNX Virtual Integrator/KNX Virtual Integrator/MainWindow.xaml.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace KNX_Virtual_Integrator;
+﻿using System.Windows.Media;
+
+namespace KNX_Virtual_Integrator;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -8,5 +10,26 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    
+    /// <summary>
+    /// Converts a string representation of a color to a SolidColorBrush.
+    /// </summary>
+    /// <param name="colorInput">The string representation of the color (e.g., "#RRGGBB" or "ColorName").</param>
+    /// <returns>A SolidColorBrush representing the converted color.</returns>
+    public static SolidColorBrush ConvertStringColor(string colorInput)
+    {
+        return new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorInput));
+    }
+
+    public void ApplyScaling(float scaleFactor)
+    {
+        App.ConsoleAndLogWriteLine("MainWindow.ApplyScaling is not implemented");
+    }
+
+    public void UpdateWindowContents(bool b, bool b1, bool b2)
+    {
+        App.ConsoleAndLogWriteLine("MainWindow.UpdateWindowContents is not implemented");
     }
 }
