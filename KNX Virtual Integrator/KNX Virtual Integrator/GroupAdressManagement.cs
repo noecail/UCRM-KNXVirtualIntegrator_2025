aa95d7d22f75b83043eb243f4ca55086525f1b9b
emmacstn <couston@insa-toulouse.fr>
2024-08-08 10:32:56 +0200

    groupe les adresses en fonction des noms

--- KNX Virtual Integrator/KNX Virtual Integrator/GroupAdressManagement.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/GroupAdressManagement.cs
@@ -30,6 +30,38 @@ public class GroupAddressManagement
                         return name != null && name.StartsWith("Cmd",StringComparison.OrdinalIgnoreCase);
                     })
                     .ToList();
+                
+                // Dictionnaire pour regrouper les éléments par nom commun sans préfixe
+                var groupedAddresses = new Dictionary<string, List<XElement>>();
+
+                foreach (var ie in ieGroupAddresses)
+                {
+                    var name = (string)ie.Attribute("Name");
+                    if (name != null)
+                    {
+                        var commonName = name.Substring(2); // Supprimer le préfixe "Ie"
+                        if (!groupedAddresses.ContainsKey(commonName))
+                        {
+                            groupedAddresses[commonName] = new List<XElement>();
+                        }
+                        groupedAddresses[commonName].Add(ie);
+                    }
+                }
+
+                foreach (var cmd in cmdGroupAddresses)
+                {
+                    var name = (string)cmd.Attribute("Name");
+                    if (name != null)
+                    {
+                        var commonName = name.Substring(3); // Supprimer le préfixe "Cmd"
+                        if (!groupedAddresses.ContainsKey(commonName))
+                        {
+                            groupedAddresses[commonName] = new List<XElement>();
+                        }
+                        groupedAddresses[commonName].Add(cmd);
+                    }
+                }
+
             }
         }
         else
