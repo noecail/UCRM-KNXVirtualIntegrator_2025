649890e6c473c0d8d8db86c228ad902d368da44e
louisont12 <tetaz@insa-toulouse.fr>
2024-08-12 15:37:09 +0200

    connexion ip inter

--- KNX_PROJET_2/KNX_PROJET_2/MainWindow.xaml.cs
+++ KNX_PROJET_2/KNX_PROJET_2/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using GalaSoft.MvvmLight.CommandWpf;
 
 
 using GalaSoft.MvvmLight;
+using System.Threading;
 
 namespace KNX_PROJET_2
 {
@@ -28,7 +29,15 @@ namespace KNX_PROJET_2
         public MainWindow()
         {
             InitializeComponent();
+            _cancellationTokenSource = new CancellationTokenSource();
+            IsBusy = false;
+            this.Loaded += MainWindow_Loaded;
         }
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await DiscoverInterfacesAsync();
+        }
+    
 
 
         //ATTRIBUTS
@@ -128,7 +137,7 @@ namespace KNX_PROJET_2
         private KnxBus _bus;
         private CancellationTokenSource _cancellationTokenSource;
 
-        public bool IsBusy => _cancellationTokenSource?.Token.IsCancellationRequested == false;
+        public bool IsBusy { get; private set; }
         public bool IsConnected => _bus != null && _bus.ConnectionState == BusConnectionState.Connected;
 
         //Gestion du clic sur le bouton Connect
@@ -302,24 +311,39 @@ namespace KNX_PROJET_2
                 // Découverte des interfaces IP
                 var ipDiscoveryTask = Task.Run(async () =>
                 {
-                    var results = KnxBus.DiscoverIpDevicesAsync(CancellationToken.None);
-                    await foreach (var result in results)
+                    using (var cts = new CancellationTokenSource())
                     {
-                        foreach (var tunnelingServer in result.GetTunnelingConnections())
+                        var results = KnxBus.DiscoverIpDevicesAsync(cts.Token);
+                        await foreach (var result in results)
                         {
-                            discoveredInterfaces.Add(new InterfaceViewModel(
-                                ConnectorType.IpTunneling,
-                                tunnelingServer.Name,
-                                tunnelingServer.ToConnectionString()));
-                        }
+                            // Traitement des connexions tunneling
+                            foreach (var tunnelingServer in result.GetTunnelingConnections())
+                            {
+                                // Mettre à jour les connexions existantes ou ajouter de nouvelles connexions
+                                if (result.IsExtensionOf != null)
+                                {
+                                    var existing = discoveredInterfaces.FirstOrDefault(i => i.ConnectionString == result.IsExtensionOf.ToConnectionString());
+                                    if (existing != null)
+                                    {
+                                        existing.ConnectionString = tunnelingServer.ToConnectionString();
+                                        continue;
+                                    }
+                                }
 
-                        if (result.Supports(ServiceFamily.Routing, 1))
-                        {
-                            var routingParameters = IpRoutingConnectorParameters.FromDiscovery(result);
-                            discoveredInterfaces.Add(new InterfaceViewModel(
-                                ConnectorType.IpRouting,
-                                $"{result.MulticastAddress} on {result.LocalIPAddress}",
-                                routingParameters.ToConnectionString()));
+                                var displayName = tunnelingServer.IndividualAddress.HasValue
+                                    ? $"{tunnelingServer.Name} {tunnelingServer.HostAddress} ({tunnelingServer.IndividualAddress.Value})"
+                                    : $"{tunnelingServer.Name} {tunnelingServer.HostAddress}";
+                                var tunneling = new InterfaceViewModel(ConnectorType.IpTunneling, displayName, tunnelingServer.ToConnectionString());
+                                discoveredInterfaces.Add(tunneling);
+                            }
+
+                            // Traitement des connexions IP routing
+                            if (result.Supports(ServiceFamily.Routing, 1))
+                            {
+                                var routingParameters = IpRoutingConnectorParameters.FromDiscovery(result);
+                                var routing = new InterfaceViewModel(ConnectorType.IpRouting, $"{result.MulticastAddress} on {result.LocalIPAddress}", routingParameters.ToConnectionString());
+                                discoveredInterfaces.Add(routing);
+                            }
                         }
                     }
                 });
@@ -329,10 +353,8 @@ namespace KNX_PROJET_2
                 {
                     foreach (var usbDevice in KnxBus.GetAttachedUsbDevices())
                     {
-                        discoveredInterfaces.Add(new InterfaceViewModel(
-                            ConnectorType.Usb,
-                            usbDevice.DisplayName,
-                            usbDevice.ToConnectionString()));
+                        var interfaceViewModel = new InterfaceViewModel(ConnectorType.Usb, usbDevice.DisplayName, usbDevice.ToConnectionString());
+                        discoveredInterfaces.Add(interfaceViewModel);
                     }
                 });
 
@@ -347,6 +369,7 @@ namespace KNX_PROJET_2
                 MessageBox.Show($"Erreur lors de la découverte des interfaces : {ex.Message}");
             }
         }
+
 
 
 
--- KNX_PROJET_2/KNX_PROJET_2/obj/Debug/net8.0-windows/KNX_PROJET_2_dbab3ihe_wpftmp.AssemblyInfo.cs
+++ KNX_PROJET_2/KNX_PROJET_2/obj/Debug/net8.0-windows/KNX_PROJET_2_dbab3ihe_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KNX_PROJET_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcf455433b53a541bfdcd66ea942024bdfd1a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fd68c3334bcb660fd89e70069b94c37b803143")]
 [assembly: System.Reflection.AssemblyProductAttribute("KNX_PROJET_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KNX_PROJET_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
