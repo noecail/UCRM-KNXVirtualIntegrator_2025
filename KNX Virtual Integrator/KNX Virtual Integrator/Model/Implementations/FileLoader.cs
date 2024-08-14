e262a9b4040574e6781448cf6fcb66070104405b
MaximeO <maxime.oliveira40@gmail.com>
2024-08-15 01:30:48 +0200

    FileLoader + interface ok

---
+++ KNX Virtual Integrator/KNX Virtual Integrator/Model/Implementations/FileLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+using KNX_Virtual_Integrator.Model.Interfaces;
+
+namespace KNX_Virtual_Integrator.Model.Implementations;
+
+public class FileLoader : IFileLoader
+{
+    /// <summary>
+    /// Loads an XML document from a specified path.
+    /// </summary>
+    /// <param name="path">The path to the XML document to load.</param>
+    /// <returns>Returns an XDocument if the file is successfully loaded; otherwise, returns null.</returns>
+    public XDocument? LoadXmlDocument(string path)
+    {
+        return XDocument.Load(path);
+    }
+}
