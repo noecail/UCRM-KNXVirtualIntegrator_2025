87e23d9f4f792f091f294c103ea576e48d33950c
louisont12 <tetaz@insa-toulouse.fr>
2024-08-19 11:13:51 +0200

    commit

--- KNX_PROJET_2/KNX_PROJET_2/MainWindowViewModel.cs
+++ KNX_PROJET_2/KNX_PROJET_2/MainWindowViewModel.cs
@@ -294,10 +294,10 @@ namespace KNX_PROJET_2
                     Application.Current.Dispatcher.Invoke(() =>
                     {
                         // Mettre à jour les interfaces pour l'utilisateur
-                        GroupAddresses.Clear();
+                        DiscoveredInterfaces.Clear();
                         foreach (var discoveredInterface in discoveredInterfaces)
                         {
-                            GroupAddresses.Add(discoveredInterface);
+                            DiscoveredInterfaces.Add(discoveredInterface);
                         }
                     });
                 }
@@ -319,10 +319,10 @@ namespace KNX_PROJET_2
                     Application.Current.Dispatcher.Invoke(() =>
                     {
                         // Mettre à jour les interfaces pour l'utilisateur
-                        GroupAddresses.Clear();
+                        DiscoveredInterfaces.Clear();
                         foreach (var discoveredInterface in discoveredInterfaces)
                         {
-                            GroupAddresses.Add(discoveredInterface);
+                            DiscoveredInterfaces.Add(discoveredInterface);
                         }
                     });
                 }
