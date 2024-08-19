e421689c9e875dd9bbf63167c8121000ce3a218c
MaximeO <maxime.oliveira40@gmail.com>
2024-08-19 10:50:41 +0200

    .

--- KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
@@ -47,7 +47,7 @@ public partial class App
     /// <summary>
     /// Represents the build of the application. Updated each time portions of code are merged on github.
     /// </summary>
-    public const int AppBuild = 87;
+    public const int AppBuild = 88;
     
         
     
@@ -151,5 +151,6 @@ public partial class App
     }
 
 }
+
 
 
