572203617b1bbc81de14be73cd72ffc1b560d82c
MaximeO <maxime.oliveira40@gmail.com>
2024-08-20 11:21:31 +0200

    Update MainViewModel.cs

---
+++ KNX Virtual Integrator/KNX Virtual Integrator/ViewModel/RelayCommandWithResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+
+namespace KNX_Virtual_Integrator.ViewModel;
+
+public class RelayCommandWithResult<T, TResult>(Func<T, TResult> execute, Func<T, bool>? canExecute = null) : ICommand
+{
+    private readonly Func<T, TResult> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+    public bool CanExecute(object? parameter)
+    {
+        // Assurez-vous que le paramètre est du type attendu
+        if (parameter is T tParameter)
+        {
+            return canExecute?.Invoke(tParameter) ?? true;
+        }
+        return false;
+    }
+
+    public TResult ExecuteWithResult(object? parameter)
+    {
+        // Assurez-vous que le paramètre est du type attendu
+        if (parameter is T tParameter)
+        {
+            return _execute(tParameter);
+        }
+        throw new ArgumentException($"Parameter is not of type {typeof(T).Name}");
+    }
+
+    public void Execute(object? parameter)
+    {
+        ExecuteWithResult(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
