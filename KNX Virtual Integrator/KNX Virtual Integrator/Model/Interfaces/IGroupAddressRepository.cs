78d1e9c8c403ff4e6d29b75485a499b92d65720f
MaximeO <maxime.oliveira40@gmail.com>
2024-08-22 18:59:52 +0200

    rename d'un fichier mal nommé

--- KNX Virtual Integrator/KNX Virtual Integrator/Model/Interfaces/IGroupAddressRepository.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/Model/Interfaces/IGroupAddressRepository.cs
@@ -2,7 +2,7 @@ using System.Xml.Linq;
 
 namespace KNX_Virtual_Integrator.Model.Interfaces;
 
-public interface IGrpAddrRepository{
+public interface IGroupAddressRepository{
 
     public List<XElement> GetGrpAddr(string key);
     
