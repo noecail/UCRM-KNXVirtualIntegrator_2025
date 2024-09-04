2d7028f355c237aa5f4c8d940cc50aa6a54a0f9a
natouney <nathan.b2470@gmail.com>
2024-09-04 13:15:06 +0200

    busconnection et groupcommunication sont super commentés maintenant

--- KNX Virtual Integrator/KNX Virtual Integrator/ViewModel/MainViewModel.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/ViewModel/MainViewModel.cs
@@ -9,6 +9,7 @@ using KNX_Virtual_Integrator.View;
 using KNX_Virtual_Integrator.ViewModel.Commands;
 using ICommand = KNX_Virtual_Integrator.ViewModel.Commands.ICommand;
 using System.ComponentModel;
+using Knx.Falcon.KnxnetIp;
 
 // ReSharper disable InvalidXmlDocComment
 // ReSharper disable NullableWarningSuppressionIsUsed
@@ -67,11 +68,11 @@ public partial class MainViewModel : ObservableObject, INotifyPropertyChanged
             ConnectBusCommand?.RaiseCanExecuteChanged();
             DisconnectBusCommand?.RaiseCanExecuteChanged();
         };
+        _busConnection.SelectedConnectionType = "Type=IP";
         
         ProjectFolderPath = "";
 
-        // Définir le type de connexion initial
-        _busConnection.SelectedConnectionType = "Type=IP";
+        
 
         
         
@@ -209,21 +210,9 @@ public partial class MainViewModel : ObservableObject, INotifyPropertyChanged
     public ICommand FindZeroXmlCommand { get; private set; }
 
     public RelayCommand OpenConnectionWindowCommand { get; }
-    
-    /// <summary>
-    /// Command that connects to the bus asynchronously.
-    /// </summary>
-    public RelayCommand ConnectBusCommand { get; private set; }
-
-    /// <summary>
-    /// Command that disconnects from the bus asynchronously.
-    /// </summary>
-    public RelayCommand DisconnectBusCommand { get; private set; } 
-
-    /// <summary>
-    /// Command that refreshes the list of bus interfaces asynchronously.
-    /// </summary>
-    public RelayCommand RefreshInterfacesCommand { get; private set; }
+    public RelayCommand ConnectBusCommand { get; }
+    public RelayCommand DisconnectBusCommand { get; }
+    public RelayCommand RefreshInterfacesCommand { get; }
 
     /// <summary>
     /// Command that sends a group value write "on" command asynchronously.
