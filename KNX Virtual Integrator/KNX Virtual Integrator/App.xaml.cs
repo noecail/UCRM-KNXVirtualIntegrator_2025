0a0e00ac6959211527bc416c1f3711ee963a8e9d
Hugo.M <151736047+MICHEL-Hugo@users.noreply.github.com>
2024-09-05 14:01:12 +0200

    Update App.xaml.cs

--- KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
@@ -193,7 +193,7 @@ public partial class App
     /// <summary>
     /// Represents the build of the application. Updated each time portions of code are merged on github.
     /// </summary>
-    public const int AppBuild = 108;
+    public const int AppBuild = 109;
     
         
     
@@ -357,6 +357,7 @@ public partial class App
     }
     
 }
+
 
 
 
