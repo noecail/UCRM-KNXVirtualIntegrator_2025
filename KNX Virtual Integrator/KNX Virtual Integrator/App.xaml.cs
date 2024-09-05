321ac5bfbd51165e3c97c0a0f563fd441b72da28
louisont12 <tetaz@insa-toulouse.fr>
2024-09-05 15:37:50 +0200

    commit

--- KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
@@ -198,7 +198,7 @@ public partial class App
     /// <summary>
     /// Represents the build of the application. Updated each time portions of code are merged on github.
     /// </summary>
-    public const int AppBuild = 110;
+    public const int AppBuild = 111;
     
         
     
@@ -362,6 +362,7 @@ public partial class App
     }
     
 }
+
 
 
 
