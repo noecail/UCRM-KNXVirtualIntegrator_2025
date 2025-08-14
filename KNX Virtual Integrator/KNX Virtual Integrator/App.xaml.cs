3b92cb5ff7b409cb9ff118bc7a80b21689c09a26
RMarques <rmarqueslpz@gmail.com>
2025-08-14 14:59:30 +0200

    Feat : Added GroupAddress view

--- KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/App.xaml.cs
@@ -198,7 +198,7 @@ public partial class App
     /// <summary>
     /// Represents the build of the application. Updated each time portions of code are merged on github.
     /// </summary>
-    public const int AppBuild = 160;
+    public const int AppBuild = 162;
     
         
     
@@ -363,48 +363,3 @@ public partial class App
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
