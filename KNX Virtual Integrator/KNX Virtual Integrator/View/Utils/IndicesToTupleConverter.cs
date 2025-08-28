1c23e53e7428d351095ae17de4afc5c5e227ac8a
noecail <caillet.noe@laposte.net>
2025-08-28 12:47:56 +0200

    other: some summaries

--- KNX Virtual Integrator/KNX Virtual Integrator/View/Utils/IndicesToTupleConverter.cs
+++ KNX Virtual Integrator/KNX Virtual Integrator/View/Utils/IndicesToTupleConverter.cs
@@ -3,8 +3,20 @@ using System.Windows.Data;
 
 namespace KNX_Virtual_Integrator.View.Utils;
 
+/// <summary>
+/// Converter class used to convert from two Int values to a Tuple
+/// Specifically used in the Structure Edit Window, delete DPT button, to pass both the element structure's index and the to-be-deleted DPT's index
+/// </summary>
 public class IndicesToTupleConverter : IMultiValueConverter
 {
+    /// <summary>
+    /// Convert from the two Int values to the Tuple
+    /// </summary>
+    /// <param name="values"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         if (values.Length == 2 
@@ -16,6 +28,15 @@ public class IndicesToTupleConverter : IMultiValueConverter
         return Binding.DoNothing;
     }
 
+    /// <summary>
+    /// Convert from the Tuple to the two Int values
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetTypes"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
